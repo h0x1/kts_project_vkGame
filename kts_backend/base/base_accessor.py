99beb99b16c4fdb3115875672004c396f3c684c4
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-10-27 15:39:50 +0300

    Add files via upload

--- kts_backend/base/base_accessor.py
+++ kts_backend/base/base_accessor.py
@@ -2,7 +2,7 @@ import typing
 from logging import getLogger
 
 if typing.TYPE_CHECKING:
-    from kts_backend.web.app import Application
+    from app.web.app import Application
 
 
 class BaseAccessor:
